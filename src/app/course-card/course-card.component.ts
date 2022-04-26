59c2799bba12991b3489657e598edce9740a68b1
Vivien Talos <vivientalos29@gmail.com>
2022-04-26 15:40:09 +0200

    create dinamic for each card, event handling and custom event is created

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { Course } from "../model/course";
 @Component({
-  selector: 'course-card',
-  templateUrl: './course-card.component.html',
-  styleUrls: ['./course-card.component.css']
+  selector: "course-card",
+  templateUrl: "./course-card.component.html",
+  styleUrls: ["./course-card.component.css"],
 })
 export class CourseCardComponent implements OnInit {
+  @Input()
+  course: Course;
 
-  constructor() { }
+  @Output()
+  courseSelected = new EventEmitter<Course>();
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onCourseViewed() {
+    console.log("button clicked");
+    this.courseSelected.emit(this.course);
   }
-
 }
