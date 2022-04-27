631649e189bceb578f0b106aacaa1f6ef26f8a38
Vivien Talos <vivientalos29@gmail.com>
2022-04-27 15:47:49 +0200

    angular built-in pipes are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,14 @@ import { Course } from "./model/course";
 export class AppComponent {
   courses = COURSES;
 
+  startDate = new Date(2000, 0, 1);
+
+  price = 9.99232532;
+
+  rate = 0.67;
+
+  course = COURSES[0];
+
   onCourseSelected(course: Course) {
     console.log("card clicked", course);
   }
